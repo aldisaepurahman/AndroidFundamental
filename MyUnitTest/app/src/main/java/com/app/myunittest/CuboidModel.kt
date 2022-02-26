abb27ca98828ab32d88edfff72c1bd90e143a543
aldisaepurahman <aldisaepurahman@gmail.com>
2022-02-26 16:25:12 +0700

    Update Projek MyUnitTest

---
+++ MyUnitTest/app/src/main/java/com/app/myunittest/CuboidModel.kt
@@ -0,0 +1,25 @@
+package com.app.myunittest
+
+class CuboidModel {
+    private var width = 0.0
+    private var length = 0.0
+    private var height = 0.0
+
+    fun getVolume(): Double = width * length * height
+
+    fun getSurfaceArea(): Double {
+        val wl = width * length
+        val wh = width * height
+        val hl = height * length
+
+        return 2 * (wl + wh + hl)
+    }
+
+    fun getCircumference(): Double = 4 * (width + length + height)
+
+    fun save(width: Double, length: Double, height: Double) {
+        this.width = width
+        this.length = length
+        this.height = height
+    }
+}

5ac3cc0b8b0486a9ef5480c79e82ea0a7ca123eb
aldisaepurahman <aldisaepurahman@gmail.com>
2022-03-02 21:48:17 +0700

    Update Proyek Repository dan Injection

---
+++ LatihanRepository/app/src/main/java/com/dicoding/newsapp/utils/AppExecutors.kt
@@ -0,0 +1,22 @@
+package com.dicoding.newsapp.utils
+
+import android.os.Handler
+import android.os.Looper
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+
+class AppExecutors {
+    val diskIO: Executor = Executors.newSingleThreadExecutor()
+    /*membagi thread ke 3 path, untuk retrofit, Room, dan executors thread itu sendiri*/
+    val networkIO: Executor = Executors.newFixedThreadPool(3)
+    val mainThread: Executor = MainThreadExecutor()
+
+    private class MainThreadExecutor : Executor {
+        private val mainThreadHandler = Handler(Looper.getMainLooper())
+
+        override fun execute(command: Runnable) {
+            mainThreadHandler.post(command)
+        }
+    }
+
+}
